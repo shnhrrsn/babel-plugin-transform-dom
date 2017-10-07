79438fe51e2bd43596c6e2949b959bc9bfbc2e6a
shnhrrsn <shnhrrsn@gmail.com>
2017-10-07 12:48:21 -0400

    Added support for SVGs

---
+++ test/fixtures/svg.dom.out.js
@@ -0,0 +1,13 @@
+var _g, _path, _path2, _path3, _path4, _path5, _path6, _path7, _path8, _path9;
+
+var _svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+_svg.setAttributeNS(null, 'width', 39);
+
+_svg.setAttributeNS(null, 'height', 52);
+
+_svg.setAttributeNS(null, 'viewBox', '0 0 39 52');
+
+_svg.appendChild((_g = document.createElementNS('http://www.w3.org/2000/svg', 'g'), _g.setAttributeNS(null, 'fill', 'none'), _g.setAttributeNS(null, 'fill-rule', 'evenodd'), _g.appendChild((_path = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path.setAttributeNS(null, 'fill', '#2DBCFF'), _path.setAttributeNS(null, 'd', 'M13 39h13v13H13z'), _path)), _g.appendChild((_path2 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path2.setAttributeNS(null, 'fill', '#5265FF'), _path2.setAttributeNS(null, 'd', 'M0 26h13v13H0z'), _path2)), _g.appendChild((_path3 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path3.setAttributeNS(null, 'fill', '#10CFFF'), _path3.setAttributeNS(null, 'd', 'M0 13h13v13H0z'), _path3)), _g.appendChild((_path4 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path4.setAttributeNS(null, 'fill', '#4D7BFF'), _path4.setAttributeNS(null, 'd', 'M13 0h13v13H13z'), _path4)), _g.appendChild((_path5 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path5.setAttributeNS(null, 'fill', '#4F40FF'), _path5.setAttributeNS(null, 'd', 'M29.25 39H39V26H26v9.75z'), _path5)), _g.appendChild((_path6 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path6.setAttributeNS(null, 'fill', '#498CFF'), _path6.setAttributeNS(null, 'd', 'M35.75 52H26V39h13v9.75z'), _path6)), _g.appendChild((_path7 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path7.setAttributeNS(null, 'fill', '#3DA6FF'), _path7.setAttributeNS(null, 'd', 'M35.75 0H26v13h13V3.25z'), _path7)), _g.appendChild((_path8 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path8.setAttributeNS(null, 'fill', '#4C82FF'), _path8.setAttributeNS(null, 'd', 'M3.25 52H13V39H0v9.75z'), _path8)), _g.appendChild((_path9 = document.createElementNS('http://www.w3.org/2000/svg', 'path'), _path9.setAttributeNS(null, 'fill', '#5633FF'), _path9.setAttributeNS(null, 'd', 'M3.25 0H13v13H0V3.25'), _path9)), _g));
+
+_svg;
